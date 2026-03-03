d739dae4c97ef10cd8d45f756c9913bf11bc777d
JuanjoDior <JuanjoDior@users.noreply.github.com>
2026-03-03 13:44:10 +0000

    deploy: b4283c201e3e8fadb086851340aa0f4a48d56432

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3412577606" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "50468249" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
